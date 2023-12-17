e526bced044f3ee7eceb52c71ffc27ac2f31c6bc
Aaron Echler <acechler@gmail.com>
2023-12-17 16:08:24 -0500

    Created a datastructure directory with a graph object.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
 
-#from pygame_stuff.pygame_driver import PygameDriver
+from pygame_stuff.pygame_driver import PygameDriver
 from terminal_stuff.terminal_driver import TerminalDriver
 
 
 def main():
     # Initialize and run the pygame instance
-    # driver = PygameDriver()
-    # driver.run()
-     driver = TerminalDriver()
-     driver.run()
+     #driver = PygameDriver()
+     #driver.run()
+      driver = TerminalDriver()
+      driver.run()
 
 if __name__ == "__main__":
     main()
--- terminal_stuff/terminal_driver.py
+++ terminal_stuff/terminal_driver.py
@@ -1,16 +1,25 @@
 import sys
 from termcolor import colored
+from datastructures import Graph, Node
+
+
+class DecisionTest:
+    def __init__(self):
+      pass
+    def run(self):
+       values = [1,2,3,4,5,6,7,8,9,10]
+       for i in values:
+              if i % 2 == 0:
+                print("Not Weird")
+              else:
+                print("Weird")
+ 
 
 class TerminalDriver:
     def __init__(self):
         pass
+        
 
     @staticmethod
     def run():
-       
-       values = [1,2,3,4,5,6,7,8,9,10]
-       for i in values:
-              if i % 2 == 0:
-                print(colored(i, 'green'), end=" ")
-              else:
-                print(colored(i, 'red'), end=" ")
+       print(colored("Hello World",'green'),end=" ")
